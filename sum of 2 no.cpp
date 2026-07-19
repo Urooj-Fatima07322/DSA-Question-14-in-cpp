42b3c8e0f97af0a956b1ccd106fa605787f1c286
Urooj Fatima <uroojanees2007@gmail.com>
2026-07-19 14:56:58 +0500

    Add files via upload

---
+++ sum of 2 no.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	float a;
+	float b;
+	float result=a+b;
+	cout<<"Enter your first no. "<<endl;
+	cin>>a;
+	cout<<"Enter your second no. "<<endl;
+	cin>>b;
+	cout<<"The result is "<<result<<endl;
+	return 0;
+}
